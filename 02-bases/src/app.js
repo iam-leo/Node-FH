0e476327446a979182f6e8297649800544d3d9d5
Leo Ss <leo.vm.cba@gmail.com>
2025-08-25 20:20:26 -0300

    ✅ /bases: agregar importaciones y exportaciones

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,3 +1,8 @@
 // Punto de entrada de la aplicacion
 
+const { emailTemplate } = require('./js-foundation/01-template');
+
+
 console.log("Hola mundo desde app.js!");
+
+console.log( emailTemplate );
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,10 @@
+const emailTemplate =  `
+    <div>
+        <h1>Hola {{name}}!</h1>
+        <p>Gracias por registrarte en nuestro sitio web.</p>
+    </div>
+    `
+
+module.exports = {
+    emailTemplate
+}
