bf552ff455a43fa5a6b0e8e3c9580ded617ea0d6
Leo Ss <leo.vm.cba@gmail.com>
2025-09-02 20:22:00 -0300

    ✅ /bases: factory functions necesidad

--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,11 +1,16 @@
 //  NOTE: Un factory function es una función que genera otra función.
 
+const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age');
+
 const buildPerson = ( {name, birthday} ) => {
     return {
-        id: Math.floor(Math.random() * 1000), // Simula un ID unico
+        //id: Math.floor(Math.random() * 1000), // Simula un ID unico
+        id: uuidv4(), // Genera un ID unico con la libreria uuid
         name,
         birthday,
-        age: new Date().getFullYear() - new Date(birthday).getFullYear()
+        //age: new Date().getFullYear() - new Date(birthday).getFullYear();
+        age: getAge(birthday)
     }
 }
 
