893a76d0e50493479dd23b2b7753dc0b05153e52
Leo Ss <leo.vm.cba@gmail.com>
2025-09-29 21:27:09 -0300

    ✅ /bases: axios - peticion http

--- 02-bases/src/plugins/http-client.plugin.js
+++ 02-bases/src/plugins/http-client.plugin.js
@@ -1,7 +1,19 @@
-const httpClientPlugin = {
+const axios = require('axios');
+
+// Codigo inicial del curso
+
+/* const httpClientPlugin = {
     get: async ( url ) => {
         const resp = await fetch( url );
         return await resp.json(); // Retornamos el JSON directamente de la respuesta fetch (data)
+    },
+} */
+
+// >> Clase video 37
+const httpClientPlugin = {
+    get: async ( url ) => {
+        const { data } = await axios.get( url );
+        return data; // Retornamos el JSON directamente de la respuesta axios (data)
     },
 }
 
