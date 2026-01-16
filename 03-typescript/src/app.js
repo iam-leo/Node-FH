a55b205dab1e2f1de12c0d6bacae0e16ad6c41ac
Leo Ss <leo.vm.cba@gmail.com>
2026-01-16 10:24:28 -0300

    ✅ /bases: inicio proyecto con typescript

---
+++ 03-typescript/src/app.js
@@ -0,0 +1,15 @@
+const heroes = [
+  { id: 1, name: 'Superman', owner: 'DC' },
+  { id: 2, name: 'Batman', owner: 'DC' },
+  { id: 3, name: 'Ironman', owner: 'Marvel' },
+  { id: 4, name: 'Spiderman', owner: 'Marvel' },
+  { id: 5, name: 'Hulk', owner: 'Marvel' },
+]
+
+const findHeroById = (id) => {
+  return heroes.find(hero => hero.id === id);
+}
+
+// Example usage:
+const hero = findHeroById(2);
+console.log(hero); // { id: 3, name: 'Ironman', owner: 'Marvel' }
